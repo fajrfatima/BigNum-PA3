c519ac25227a87737d244b969b52b91067624d68
Fajr Fatima <fajr.s2004@gmail.com>
2024-11-11 00:22:30 +0500

    functions

--- BigNum.cpp
+++ BigNum.cpp
@@ -16,6 +16,45 @@ BigNum :: BigNum(int num){
 }
 //ASSIGNMENT CODE
 
+BigNum& BigNum :: operator=(const BigNum& other) {
+    negative = other.negative;
+    numbers = other.numbers;
+    return *this;
+}
+
+BigNum& BigNum :: operator=(int num){
+    negative = (num < 0);
+    numbers.clear();
+
+    if(num < 0){
+        num = -num;
+    }
+}
+
+bool BigNum :: operator==(const BigNum& other) const {
+    return negative == other.negative && numbers == other.numbers;
+}
+
+bool BigNum :: operator==(int other) const {
+    return *this == BigNum(other);
+}
+
+bool operator==(int lhs, const BigNum& rhs) {
+    return BigNum(lhs) == rhs;
+}
+
+bool BigNum :: operator!=(const BigNum& other) const {
+    return !(*this == other);
+}
+
+bool BigNum :: operator!=(int other) const {
+    return !(*this == other);
+}
+
+bool operator!=(int lhs, const BigNum& rhs) {
+    return BigNum(lhs) != rhs;
+}
+
 
 //COMPARISON CODE
 //comparison operator: == and !=
